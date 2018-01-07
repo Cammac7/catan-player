d7398d26791a52caac7b912cc8c78447b065474c
Cameron <cam.mac@gmx.com>
2018-01-07 16:43:21 -0500

    added play function

--- catanClasses.py
+++ catanClasses.py
@@ -1,16 +1,16 @@
-class Card:
-    def __init__(self, ctype):
-        self.name = ctype
-
-    def play(self):
-        raise Exception("Not Implemented")
-
-class Monopoly(Card):
-    def __init__(self):
-        Card.__init__(self, "Monopoly")
-
-    def play(self):
-
+#class Card:
+#    def __init__(self, ctype):
+#        self.name = ctype
+#
+#    def play(self):
+#        raise Exception("Not Implemented")
+#
+#class Monopoly(Card):
+#    def __init__(self):
+#        Card.__init__(self, "Monopoly")
+#
+#    def play(self):
+#
 
 
 class Node:
@@ -21,21 +21,16 @@ class Node:
         self.returns = {} #dict of num:resource, or probability of resource
         self.neighbors = {} #{neighborNode:edgeColor}
 
+
 class Player:
     def __init__(self, color):
-        self.hand = {} #resouce cards
+        self.hand = Counter() #resouce cards
         self.cards = [] #development cards
         self.color = color #player color
         self.nodes = {} #(x,y):node
         self.edges = [] #do we need this?
         self.victoryPoints = 0 #maybe can contain decimals to represent probability
         self.remaining = {} #remaining roads, settlements and cities
-
-    def diceroll(self):
-        die1 = random.randint(1,6)
-        die2 = random.randint(1,6)
-        print(die1+die2)
-        return die1+die2
 
     def updateVPs(self):#do I need this function? could update for each action
         newVP = 0
@@ -48,6 +43,24 @@ class Player:
                 newVP += 2
         self.victoryPoints = newVP
 
+    def initialTurn(self):
+        raise Exception("Not Implemented")
+
+
+class Human(Player):
+    def __init__(self, color):
+        Player.__init__(self, color)
+
+class Computer(Player):
+    def __init__(self, color):
+        Player.__init__(self, color)
+# don't need dice roll, use board dice
+#    def diceroll(self):
+#        die1 = random.randint(1,6)
+#        die2 = random.randint(1,6)
+#        print(die1+die2)
+#        return die1+die2
+
 
 class CatanBoard:
     def __init__(self):
@@ -55,6 +68,31 @@ class CatanBoard:
         self.edgelist = {} #edgename:color ?? or color:[(locA,locB)]
         self.players = {} #color:player
         self.deck = [] #stack of dev cards
+
+    def play():
+    #Setup
+        self.setTerrain(self.buildTileList())
+        #Assign player colors
+        players = input("Enter color of other players in clockwise order, starting to your left (comma separated like red,white,orange): )")
+        clrList = players.split(",")
+        self.addPlayers(clrList)
+        compColor = input("Which color am I playing as? ")
+        self.players[compColor] = Player(compColor)
+        first = input("who is going first? (put their color)")
+
+    def buildTileList():
+        tList = []
+        lMap = {'g':'grain','b':'brick','o':'ore','l':'lumber','w':'wool','d':'dese    rt'}
+        while True:
+            com = input('Next Tile:')
+            if com == "build":
+                return tList
+            if com == "undo":
+                tList.pop()
+            else:
+                resource = lMap[com[-1:]]
+                number = int(com[0:-1])
+                tList.append((resource,number))
 
     def addPlayers(colorList):
         for color in colorList:
@@ -82,6 +120,10 @@ class CatanBoard:
         toNode = nodelist[toLoc]
         fromNode.neighbors[toNode] = color
         toNode.neighbors[fromNode] = color
+
+    def buildDev(self, color):
+        print("build dev card")
+        #subtract resources, add dev card to hand
         
     def setTerrain(self,tileList):
         #list tiles left->right and top->bottom
@@ -103,8 +145,6 @@ class CatanBoard:
                 for adj in [(loc[0]+1,loc[1]-1),(loc[0],loc[1]-2),(loc[0]-1,loc[1]-1),(loc[0]-1,loc[1]+1),(loc[0],loc[1]+2),(loc[0]+1,loc[1]+1)]:
                     if adj in self.nodelist:
                         self.nodelist[loc].neighbors[self.nodelist[adj]] = None #assign adjacent neighbors
-
-
 
 
 #tileList given left->right top->bottom

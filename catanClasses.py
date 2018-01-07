0e96a538935687aaca226ed42bdedec4308553e9
Cameron <cam.mac@gmx.com>
2018-01-06 19:39:28 -0500

    added build settlement and update VPs

--- catanClasses.py
+++ catanClasses.py
@@ -1,19 +1,20 @@
 class Node:
     def __init__(self):
         self.owner = None #color, or none
-        self.structure = None#city, settlement, none
+        self.structure = 0#0, 1 for settlement,2 for city none
         self.port = None #port type or None
         self.returns = {} #dict of num:resource, or probability of resource
         self.neighbors = {} #{neighborNode:edgeColor}
 
-class player:
+class Player:
     def __init__(self, color):
         self.hand = set() #resouce cards
         self.cards = [] #development cards
         self.color = color #player color
         self.nodes = {} #(x,y):node
         self.edges = [] #do we need this?
-        self.victoryPoints = 0 #can contain decimals to represent probability
+        self.victoryPoints = 0 #maybe can contain decimals to represent probability
+        self.remaining = {} #remaining roads, settlements and cities
 
     def diceroll(self):
         die1 = random.randint(1,6)
@@ -21,16 +22,50 @@ class player:
         print(die1+die2)
         return die1+die2
 
+    def updateVPs(self):#do I need this function? could update for each action
+        newVP = 0
+        for node in self.nodes.values():
+            newVP += node.structure
+        for card in self.cards:
+            if card == 'VP':
+                newVP += 1
+            elif card == 'Longest Road' or card == 'Largest Army':
+                newVP += 2
+        self.victoryPoints = newVP
 
-class catanBoard:
+
+class CatanBoard:
     def __init__(self):
         self.nodelist = {} #location:node. 54 total nodes
-        self.edgelist = {} #edgename:color ??
+        self.edgelist = {} #edgename:color ?? or color:[(locA,locB)]
         self.players = {} #color:player
         self.deck = [] #stack of dev cards
 
+    def addPlayers(colorList):
+        for color in colorList:
+            self.players[color] = Player(color)
+
     def addNode(self,location):
         self.nodelist[location] = Node()
+
+    def buildSettle(self,color,location):
+        selecNode = self.nodelist[location]
+        selecNode.owner = color
+        selecNode.structure = 1
+        player = self.players[color]
+        player.nodes[location] = selecNode
+        player.victoryPoints += 1 #this is faster than running the function
+
+    def buildCity(self, location):
+        selecNode = self.nodelist[location]
+        selecNode.structure = 2
+        self.players[selecNode.owner].victoryPoints += 1
+
+    def buildRoad(self, color, fromLoc, toLoc):
+        fromNode = nodelist[fromLoc]
+        toNode = nodelist[toLoc]
+        fromNode.neighbors[toNode] = color
+        toNode.neighbors[fromNode] = color
         
     def setTerrain(self,tileList):
         #list tiles left->right and top->bottom
@@ -53,16 +88,12 @@ class catanBoard:
                     if adj in self.nodelist:
                         self.nodelist[loc].neighbors[self.nodelist[adj]] = None #assign adjacent neighbors
 
-class playCatan:
-    #dynamic init to take list of opponent colors
-    def __init__(self,....,ownColor
-
 
 
 
 #tileList given left->right top->bottom
 tileList = [('ore', 10), ('wool', 2), ('lumber', 9), ('grain', 12), ('brick', 6), ('wool', 4), ('brick', 10), ('grain', 9), ('lumber', 11), ('desert', 0), ('lumber', 3), ('ore', 8), ('lumber', 8), ('ore', 3), ('grain', 4), ('wool', 5), ('brick', 5), ('grain', 6), ('wool', 11)]
-myboard = catanBoard()
+myboard = CatanBoard()
 myboard.setTerrain(tileList)
 for node in myboard.nodelist.values():
     nears = [nb.returns for nb in node.neighbors.keys()]

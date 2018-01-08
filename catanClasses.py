6716f361b1c4b9dbb4f33e1e7f0c1351951bb46e
Cameron <cam.mac@gmx.com>
2018-01-07 21:32:18 -0500

    added rules

--- catanClasses.py
+++ catanClasses.py
@@ -98,6 +98,9 @@ class CatanBoard:
         for color in colorList:
             self.players[color] = Player(color)
 
+    def addPort(self,location,portType):
+        self.nodelist[location].port = portType
+
     def addNode(self,location):
         self.nodelist[location] = Node()
 
@@ -150,7 +153,8 @@ class CatanBoard:
 #tileList given left->right top->bottom
 tileList = [('ore', 10), ('wool', 2), ('lumber', 9), ('grain', 12), ('brick', 6), ('wool', 4), ('brick', 10), ('grain', 9), ('lumber', 11), ('desert', 0), ('lumber', 3), ('ore', 8), ('lumber', 8), ('ore', 3), ('grain', 4), ('wool', 5), ('brick', 5), ('grain', 6), ('wool', 11)]
 myboard = CatanBoard()
-myboard.setTerrain(tileList)
-for node in myboard.nodelist.values():
-    nears = [nb.returns for nb in node.neighbors.keys()]
-    print("Node is: {}. Node neighb is: {}".format(node.returns, nears))
+myboard.play()
+#myboard.setTerrain(tileList)
+#for node in myboard.nodelist.values():
+#    nears = [nb.returns for nb in node.neighbors.keys()]
+#    print("Node is: {}. Node neighb is: {}".format(node.returns, nears))

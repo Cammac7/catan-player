908f3efc1e23950474b6aea221212e757df94e48
Cameron <cam.mac@gmx.com>
2018-08-11 14:56:58 -0400

    added player functions
    and research paper

--- player.py
+++ player.py
@@ -4,6 +4,7 @@ from enum import Enum, unique
 from collections import Counter
 import re
 import random
+import datetime
 
 @unique
 class Resource(Enum):
@@ -312,6 +313,12 @@ class Human(Player):
 class Computer(Player):
     def __init__(self, color, board):
         Player.__init__(self, color, board)
+        self.states = []    #Initialize statistics table
+        self.max_moves = 10  # Max moves forward for the computer
+        self.calculation_time = datetime.timedelta(seconds=30) #Set turn time
+        self.wins = {}
+        self.plays = {}
+        
 
     def initPlace(self):
         # TODO: Make this a real function. Currently does random selection
@@ -326,7 +333,51 @@ class Computer(Player):
         self.board.buildSettle(self.color, nodeChoice)
         self.board.buildRoad(self.color, nodeChoice, roadChoice)
 
+    def update(self, state):
+        self.states.append(state)
+        #take the game state, append it to the history
+        pass
+        
+    def runSimulation(self):
+        #play out a "random" game from the current position
+        #update the statistics table with the result
+        visited_states = set()
+        states_copy = self.states[:]
+        state = states_copy[-1]
+        
+        for t in range(self.max_moves):
+            legal = elf.board.legal_plays(states_copy)
+            
+            play = choice(legal)
+            state = self.board.next_state(state, play)
+            states_copy.append(state)
+            
+            winner = self.board.winner(states_copy)
+            if winner:
+                break
+        pass 
+    
     def playTurn(self):
+        #Calculate the best move from the current game state and return it
         # TODO: Need to implement
+        # 1. Determine all possible moves
+        # 2. k random games are played out to the very end, and the scores are recorded.
+        # 3. The move leading to the best score is chosen.
+    #define weights of moves
+    #basic weight == 1
+    #building a city or settlement == +10000
+    #building a road ==
+    # 10/(10^R), where R == (AARON's Roads/AARON's settles+cities)
+    # Playing a knight == +100 if the robber is blocking AARON, +1 otherwise
+    #playing a dev card = +10
+        #1. Roll
+        roll = inValRoll("What did AARON roll?: ")
+        self.board.payout(roll)
+        #2. Trade
+            # trading is adventageous. We should trade as much as possible.
+        #3. Build
+        begin = datetime.datetime.utcnow()
+        while datetime.datetime.utcnow() - begin < self.calculation_time:
+            self.run_simulation()
         print("A.A.R.O.N. IS PASSING")
         pass
